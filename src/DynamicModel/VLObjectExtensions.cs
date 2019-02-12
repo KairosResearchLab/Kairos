0078b1ac1f84fc1566aba9380ae7a15d582e5fe8
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-02-12 13:47:18 +0100

    Collected patches from LINK kairos prototype
    Restructured library in separate vl documents

---
+++ src/DynamicModel/VLObjectExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VL.Lib.Collections;
+
+namespace VL.Core
+{
+    public static class VLObjectExtensions
+    {
+        const string PATH = "Path";
+        const string INDEX = "Index";
+
+        public static T GeneratePaths<T>(this T instance) where T : IVLObject
+        {
+            return (T)instance.GeneratePaths_("", 0);
+        }
+        /**
+         * Kairos_M
+         *   - PATH = ""
+         *   - Timeline_M
+         *     - PATH = "Timeline_M"
+         *     - Tracks
+         *       - 0
+         *         - PATH = "Timeline_M.Tracks[0]"
+         *         - Clips
+         *           - 0
+         *             - PATH = "Timeline_M.Tracks[0].Clips[0]"
+         *             - Position
+         *             - Duration
+         *             - Item_M
+         *               - Value
+         *               - Node_M
+         */
+        static IVLObject GeneratePaths_(this IVLObject instance, string currentPath, int index)
+        {
+            var typeInfo = instance.Type;
+            foreach (var property in typeInfo.Properties)
+            {
+                var name = property.Name;
+                var type = property.Type.ClrType;
+                if (name == PATH && type == typeof(string))
+                {
+                    var p = property.GetValue(instance) as string;
+                    if (p != currentPath)
+                        instance = property.WithValue(instance, currentPath);
+                    continue;
+                }
+                if (name == INDEX && type == typeof(int))
+                {
+                    var i = (int)property.GetValue(instance);
+                    if (i != index)
+                        instance = property.WithValue(instance, index);
+                    continue;
+                }
+                var propertyPath = string.IsNullOrWhiteSpace(currentPath) ? name : $"{currentPath}.{name}";
+                var value = property.GetValue(instance);
+                var vlObject = value as IVLObject;
+                if (vlObject != null)
+                {
+                    var v = vlObject.GeneratePaths_(propertyPath, 0) as IVLObject;
+                    if (v != vlObject)
+                        instance = property.WithValue(instance, v);
+                    continue;
+                }
+                var spread = value as ISpread;
+                if (spread != null)
+                {
+                    for (int i = 0; i < spread.Count; i++)
+                    {
+                        var vlSlice = spread.GetItem(i) as IVLObject;
+                        if (vlSlice != null)
+                        {
+                            var newSlice = vlSlice.GeneratePaths_($"{propertyPath}[{i}]", i) as IVLObject;
+                            if (newSlice != vlSlice)
+                                spread = spread.SetItem(i, newSlice);
+                        }
+                    }
+                    if (spread != value)
+                        instance = property.WithValue(instance, spread);
+                    continue;
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ src/DynamicModel/ValueHolder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicModel
+{
+    public struct ValueHolder : IEquatable<ValueHolder>
+    {
+        public readonly object Value;
+
+        public ValueHolder(object value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(ValueHolder other)
+        {
+            var thisValue = Value;
+            var thatValue = other.Value;
+            if (ReferenceEquals(thisValue, thatValue))
+                return true;
+            if (ReferenceEquals(thisValue, null))
+                return false;
+            return thisValue.Equals(thatValue);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ValueHolder)
+                return Equals((ValueHolder)obj);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value?.GetHashCode() ?? 0;
+        }
+
+        public static bool operator ==(ValueHolder a, ValueHolder b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueHolder a, ValueHolder b)
+        {
+            return !a.Equals(b);
+        }
+    }
+}
