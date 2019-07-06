a1cc8007c89257b1a36a57bad7b0211dcf0b1b3c
N-LAPTOP\natan <natan.sinigaglia@gmail.com>
2019-07-06 19:36:33 +0200

    - channel type now is Object.
    - created custom inspector for Channel_M<Switcher_M>
    - other stuff

--- src/DynamicModel/Enums.cs
+++ src/DynamicModel/Enums.cs
@@ -10,4 +10,55 @@ namespace Kairos.Core
     {
         Default_Value, Keep_Last_Value, Keep_Alive_Last_Source
     }
+
+    public enum KairosType
+    {
+        Boolean, Integer, Float, Color, String, Vector2, Vector3, Vector4, Matrix
+    }
+
+    public enum BooleanWidget
+    {
+        Toggle, Press, Bang
+    }
+
+    public enum IntegerWidget
+    {
+        Integer, NumericUpDown
+    }
+
+    public enum FloatWidget
+    {
+        Value, Slider, Rotary
+    }
+
+    public enum ColorWidget
+    {
+        Color
+    }
+
+    public enum StringWidget
+    {
+        TextField
+    }
+
+    public enum Vector2Widget
+    {
+        Vector2D, Slider2D
+    }
+
+    public enum Vector3Widget
+    {
+        Vector3D
+    }
+
+    public enum Vector4Widget
+    {
+        Vector4D
+    }
+
+    public enum MatrixWidget
+    {
+        Matrix
+    }
+
 }
