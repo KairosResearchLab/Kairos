93a88045b909e492159fbecc8fe05efe5efb0f16
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-08-07 23:52:44 +0200

    Timeline Generic Version without canvas clips

---
+++ src/DynamicModel/Utilities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kairos.Core
+{
+    public static class Utilities
+    {
+        public static TOut CastAsNICE<TIn, TOut>(TIn input) => (TOut)(object)input;
+    }
+}
