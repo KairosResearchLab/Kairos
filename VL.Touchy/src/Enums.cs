0c4ea52c9857986426ae1b6dbf707190a5fc4264
Natan Sinigaglia <natan.sinigaglia@gmail.com>
2022-12-22 15:09:52 +0100

    wip renaming VL.Kairos.Gesture to VL.Touchy

--- VL.Touchy/src/Enums.cs
+++ VL.Touchy/src/Enums.cs
@@ -4,16 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace GestureEngine.Utils
+namespace Touchy.Utils
 {
     public enum GestureState
     {
         Start, Update, Finish, Abort
     }
-    public enum GestureEvaluation
-    {
-        On_Notify, On_Render, On_Mainloop, On_CustomEvent
-    }
 }
-
