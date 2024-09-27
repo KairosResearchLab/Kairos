7cf1980c990eed17c3d2b2520bdf8c2fa7163235
natan-sinigaglia <natan.sinigaglia@gmail.com>
2024-09-28 01:16:00 +0200

    renamed VL.LayerX to VL.LayerX.Runtime. Created Vl.LayerX.HDE. Created VL.LayerX package and entry patch

---
+++ VL.AlchemX/src/AlchemX/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AlchemX.Domain
+{
+    public enum MappingType
+    {
+        Float, Clamp, Mirror, Wrap
+    }
+}
--- VL.Kairos.Runtime/src/VL.Kairos.Runtime.CSharp/Enums.cs
+++ VL.Kairos.Runtime/src/VL.Kairos.Runtime.CSharp/Enums.cs
@@ -5,21 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Kairos.Utils
-{
-    public enum MappingType
-    {
-        Float, Clamp, Mirror, Wrap
-    }
-}
-
 namespace Kairos.Runtime
 {
-    public enum FallbackOption
-    {
-        Background, Latest
-    }
-
+   
     public enum TrackItemType
     {
         ConstantValue, LiveValue, Layer
